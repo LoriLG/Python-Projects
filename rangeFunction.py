31a97d865f609be886cca6b66b236a2609bf039f
LLG52931@gmail.com <LLG52931@gmail.com>
2017-10-04 18:23:53 -0700

    Added Range Function Assignment

---
+++ rangeFunction.py
@@ -0,0 +1,22 @@
+# Use Python range() function with one parameter to display 0 1 2 3 
+for x in range(4):
+    print(x)
+
+
+# Use Python range() function with 3 parameters to display 3 2 1 0
+
+
+
+for y in range(3,-1,-1):
+    print(y)
+    
+
+
+# Use Python range() function with 3 parameters to display 8 6 4 2    
+    
+for n in range(8,1,-2):
+    print(n)
+
+
+
+
